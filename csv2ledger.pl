987a7dc75e2bb6e3e1aefe14c95ac5f952f9dffb
coffee-and-code <mattseburn@gmail.com>
2012-05-08 22:39:20 -0400

    use config::auto

--- csv2ledger.pl
+++ csv2ledger.pl
@@ -11,6 +11,8 @@ use warnings;
 use Config::Auto;
 use Data::Dump;
 
+use feature "switch";
+
 # trims whitespace from all arguments
 
 sub trim {
@@ -51,21 +53,28 @@ sub usage {
 
 sub processConfig {
     my $config = shift;
-    my $account = shift;
-    my ($account_def, $rules);
+    my $account_name = shift;
+    my %account_def;
 
     my @accounts = @{$config->{accounts}};
     my @global_rules = @{$config->{rules}};
 
     my (@account_rules, @combined_rules);
 
-    foreach (@accounts) {
-        if ($_->{name} eq $account) {
-            foreach my $key (sort keys %$_) {
-                if ($key ne "rules") {
-                    $account_def->{$key} = $_->{$key};
-                } else {
-                    @account_rules = @{$_->{$key}};
+    foreach my $account (@accounts) {
+        if ($account->{name} eq $account_name) {
+            foreach my $key (sort keys %$account) {
+                given ($key) {
+                    when ("rules") {
+                        @account_rules = @{$account->{$key}};
+                    } when ("columnOrder") {
+                        my @columns = @{$account->{columnOrder}};
+                        foreach my $i (0 .. $#columns) {
+                            $account_def{columns}{$columns[$i]} = $i;
+                        }
+                    } default {
+                        $account_def{$key} = $account->{$key};
+                    }
                 }
             }
         }
@@ -74,7 +83,7 @@ sub processConfig {
     @combined_rules = (@global_rules, @account_rules);
 
     my $result = {
-        account => $account_def,
+        account => \%account_def,
         rules => \@combined_rules
     };
 
@@ -92,52 +101,50 @@ sub processConfig {
 # returns:
 #	(string)	string representing @input in ledger format
 
-#sub processTransactions {
-#	my $account_1 = shift;
-#	my $input = shift;
-#	my $config = shift;
-#	my @transactions;
-#
-#	foreach (@$input) {
-#		my $account_2 = "Unmarked";
-#		my @bits = split /,/;
-#		my $transaction = "";
-#
-#		my $rawDate = $bits[$config{account}{date}];
-#		my $dateFormat = $config{account}{dateFormat};
-#
-#		my $date = formatDate $rawDate, $dateFormat;
-#		my $debit = $bits[$config{account}{debit}];
-#		my $credit = $bits[$config{account}{credit}];
-#		my $label = $bits[$config{account}{label}];
-#
-#		$label =~ s/[\t\ ]+/\ /g;
-#
-#		$credit = 0 if ($credit eq "");
-#		$debit = 0 if ($debit eq "");
-#
-#		my $total = $debit - $credit;
-#
-#		while (my ($acct, $keys) = each %{$config{rules}}) {
-#			my @keys = @{$keys};
-#
-#			foreach (@keys) {
-#				if ($label =~ /$_/i) {
-#					$account_2 = $acct;
-#					last;
-#				}
-#			}
-#		}
-#
-#		$transaction .= "$date $label\n";
-#		$transaction .= "  $account_2  \$$total\n";
-#		$transaction .= "  $account_1\n";
-#
-#		push @transactions, $transaction;
-#	}
-#
-#	return join "", @transactions;
-#}
+sub processTransactions {
+	my $account_1 = shift;
+	my $input = shift;
+	my $config = shift;
+	my @transactions;
+
+	foreach (@$input) {
+		my $account_2 = "Unmarked";
+		my @bits = split /,/;
+		my $transaction = "";
+
+		my $rawDate = $bits[$config->{account}{columns}{date}];
+		my $dateFormat = $config->{account}{dateFormat};
+
+		my $date = formatDate $rawDate, $dateFormat;
+		my $debit = $bits[$config->{account}{columns}{debit}];
+		my $credit = $bits[$config->{account}{columns}{credit}];
+		my $label = $bits[$config->{account}{columns}{label}];
+
+		$label =~ s/[\t\ ]+/\ /g;
+
+		$credit = 0 if ($credit eq "");
+		$debit = 0 if ($debit eq "");
+
+		my $total = $debit - $credit;
+
+        foreach my $rule (@{$config->{rules}}) {
+            foreach (@{$rule->{keyWords}}) {
+                if ($label =~ /$_/i) {
+                    $account_2 = $rule->{account};
+                    last;
+                }
+            }
+		}   
+
+		$transaction .= "$date $label\n";
+		$transaction .= "  $account_2  \$$total\n";
+		$transaction .= "  $account_1\n";
+
+		push @transactions, $transaction;
+	}
+
+	return join "", @transactions;
+}
 
 # main starts here
 
@@ -157,7 +164,7 @@ if (! -e $inputFile) {
 
 my $config = processConfig Config::Auto::parse(), $account_1;
 
-dd($config);
+#dd($config);
 
 open $fh, $inputFile or die $!;
 my @input = <$fh>;
@@ -165,4 +172,4 @@ close $fh;
 
 trim @input;
 
-#print processTransactions $account_1, \@input, $config;
+print processTransactions $account_1, \@input, $config;
